4c2908ebc8258899188aa1f552de509260c5d9d4
Charles Watson <sircharleswatson@gmail.com>
2016-12-24 08:29:20 -0600

    serve index.html all the time for SPAs

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ module.exports = function(config) {
       res.end()
     } else {
       req.addListener('end', function () {
-        file.serve(req, res)
+        file.serve(request, response, function (e, res) {
+          file.serveFile('./index.html', 200, {}, request, response);
+        });
       }).resume()
     }
   }).listen(port)
