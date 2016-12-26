9810a862ac818f7ac33dedf2a955208cc46e0b9e
Charles Watson <sircharleswatson@gmail.com>
2016-12-26 04:29:36 -0600

    redirect http to https

--- index.js
+++ index.js
@@ -11,19 +11,25 @@ module.exports = function(config) {
   http.createServer(function(request, response){
     var credentials = auth(request)
     var unauthenticated = !credentials || credentials.name !== username || credentials.pass !== password
-    if (unauthenticated && process.env.NODE_ENV == 'production') {
-      response.writeHead(401, {
-        'WWW-Authenticate': 'Basic realm="' + config.realm + '"'
-      })
+    
+    if (request.headers["x-forwarded-proto"] !== "https") {
+      response.writeHead(301, { "Location": "https://" + request.headers["host"] + request.url })
       response.end()
     } else {
-      request.addListener('end', function () {
-        file.serve(request, response, function (e, res) {
-          if (e && (e.status === 404)) { // If the file wasn't found
-            file.serveFile('/index.html', 200, {}, request, response);
-          }
-        });
-      }).resume()
+      if (unauthenticated && process.env.NODE_ENV == 'production') {
+        response.writeHead(401, {
+          'WWW-Authenticate': 'Basic realm="' + config.realm + '"'
+        })
+        response.end()
+      } else {
+        request.addListener('end', function () {
+          file.serve(request, response, function (e, res) {
+            if (e && (e.status === 404)) { // If the file wasn't found
+              file.serveFile('/index.html', 200, {}, request, response);
+            }
+          });
+        }).resume()
+      }
     }
   }).listen(port)
 }
